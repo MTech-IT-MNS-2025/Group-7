8f52f5a6d1c828d223f6b253e8a4db3316ae8002
nidhijain64 <nidhijain3021@gmail.com>
2025-11-09 15:36:42 +0530

    Create login.js

---
+++ Assignment-3/pages/api/login.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcryptjs";
+import { dbConnect } from "../../lib/db";
+import User from "../../models/User";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  await dbConnect();
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ error: "Invalid username or password" });
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) return res.status(400).json({ error: "Invalid username or password" });
+
+  res.status(200).json({ message: "Login successful" });
+}
