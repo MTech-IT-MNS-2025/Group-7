a60d407dd3ffefe0c9b85a463143a05d3912966e
nidhijain64 <nidhijain3021@gmail.com>
2025-12-01 23:58:27 +0530

    Update Message.js

--- Assignment5/models/Message.js
+++ Assignment5/models/Message.js
@@ -1,1 +1,14 @@
+import mongoose from 'mongoose';
 
+const MessageSchema = new mongoose.Schema({
+  sender: { type: String, required: true },
+  receiver: { type: String, required: true },
+  encryptedData: {
+    cipherText: { type: String, required: true },   // AES Encrypted Content
+    nonce: { type: String, required: true },        // AES IV
+    kemCipherText: { type: String, required: true } // Kyber Encapsulated Key
+  },
+  timestamp: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+export default mongoose.models.Message || mongoose.model('Message', MessageSchema);
