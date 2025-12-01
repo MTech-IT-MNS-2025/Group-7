ed11b1d950324591539867c0fe1dbb8b9007ca23
nidhijain64 <nidhijain3021@gmail.com>
2025-12-01 23:59:18 +0530

    Update [username].js

--- Assignment5/pages/api/user/[username].js
+++ Assignment5/pages/api/user/[username].js
@@ -1,1 +1,9 @@
+import { dbConnect } from "../../../lib/db";
+import User from "../../../models/User";
 
+export default async function handler(req, res) {
+  await dbConnect();
+  const user = await User.findOne({ username: req.query.username });
+  if (!user) return res.status(404).json({ error: "Not found" });
+  res.status(200).json({ publicKey: user.publicKey });
+}
